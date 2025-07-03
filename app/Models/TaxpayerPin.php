775aae7e5c6d760e64ccddc39c59a3e5157d206a
Simiyu Samuel <98589218+simiyu-samuel@users.noreply.github.com>
2025-07-03 16:29:08 +0300

    Added logic for TaxpayerPin model

--- app/Models/TaxpayerPin.php
+++ app/Models/TaxpayerPin.php
@@ -1,10 +1,16 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TaxpayerPin extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['pin', 'name', 'address', 'is_active'];
+
+    public function kraDevices()
+    {
+        return $this->hasMany(KraDevice::class);
+    }
 }

d753c010730cf9e90babb40379f5419a1c6c4dc9
Simiyu Samuel <98589218+simiyu-samuel@users.noreply.github.com>
2025-07-03 16:27:35 +0300

    Correct namespace declaration

--- app/Models/ApiClient.php
+++ app/Models/ApiClient.php
@@ -1,6 +1,6 @@
-// app/Models/ApiClient.php
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;

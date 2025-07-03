f750de21eff3dc07bef32ccd91f77114dfd2f58b
Simiyu Samuel <98589218+simiyu-samuel@users.noreply.github.com>
2025-07-03 16:14:09 +0300

    Add migration content for kra requests table

--- database/migrations/2025_07_03_130820_create_queued_kra_requests_table.php
+++ database/migrations/2025_07_03_130820_create_queued_kra_requests_table.php
@@ -11,10 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // ...
         Schema::create('queued_kra_requests', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('kra_device_id')->nullable()->constrained('kra_devices');
+            $table->foreignUuid('transaction_id')->nullable()->constrained('transactions'); // Link to transaction if related
+            $table->string('command_type')->index(); // e.g., SEND_RECEIPTITEM, SEND_ITEM
+            $table->jsonb('data_payload'); // The JSON data needed to construct the KRA XML
+            $table->enum('status', ['PENDING', 'PROCESSING', 'SUCCESS', 'FAILED'])->default('PENDING');
+            $table->integer('attempts')->default(0);
+            $table->timestamp('last_attempted_at')->nullable();
+            $table->timestamp('next_attempt_at')->nullable(); // For exponential backoff
+            $table->text('error_message')->nullable();
             $table->timestamps();
         });
+        // ...
     }
 
     /**

40bdcfd8a583a55d7b9d17b6ea0b72add44d1dea
Simiyu Samuel <98589218+simiyu-samuel@users.noreply.github.com>
2025-07-04 10:40:55 +0300

    Logic for kra config file

---
+++ config/kra.php
@@ -0,0 +1,8 @@
+<?php
+// config/kra.php
+
+return [
+    'api_sandbox_base_url' => env('KRA_API_SANDBOX_BASE_URL', 'https://etims-sbx.kra.go.ke'),
+    'api_production_base_url' => env('KRA_API_PRODUCTION_BASE_URL', 'https://etims.kra.go.ke'),
+    'vscu_jar_base_url' => env('KRA_VSCU_JAR_BASE_URL', 'http://127.0.0.1:8088'), // Default for local VSCU
+];
